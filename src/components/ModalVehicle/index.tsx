f99af18e73aac799b5d6d0d5ea13f7e6505a4248
wiigny <wigny_36@hotmail.com>
2023-06-14 13:40:37 -0300

    fix: field fuel to fuel_type

--- src/components/ModalVehicle/index.tsx
+++ src/components/ModalVehicle/index.tsx
@@ -120,8 +120,10 @@ export const ModalVehicle = ({ isOpen, onClose, edit, car }: IModalVehicleProps)
       const newObjCarSelect = {
         fipe_price: carSelected?.value,
         ...carSelected,
-        year: parseInt(carSelected!.year)
+        year: parseInt(carSelected!.year),
+        fuel_type: carSelected?.fuel
       };
+      delete newObjCarSelect.fuel;
       delete newObjCarSelect.id;
       delete newObjCarSelect.name;
 
